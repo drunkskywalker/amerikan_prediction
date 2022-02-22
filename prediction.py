9b89bf48ac177eb22d85155acfa39db480a4c04a
drunkskywalker <44293698+drunkskywalker@users.noreply.github.com>
2022-02-22 09:15:48 +0800

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 #testing
 #testing some more
 #testing even more
+#the next day...
 
 lctime = time.localtime(os.path.getmtime("MNB.pickle"))
 app.date = time.strftime("%Y-%m-%d %H:%M:%S", lctime)
